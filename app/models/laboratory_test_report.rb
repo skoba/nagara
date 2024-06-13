bcd069bb33de7a2341e2c029db1035fb6166608a
KOBAYASHI Shinji <skoba@moss.gr.jp>
2024-06-13 21:38:33 +0900

    making view of laboratory test reports

--- app/models/laboratory_test_report.rb
+++ app/models/laboratory_test_report.rb
@@ -12,6 +12,31 @@ class LaboratoryTestReport
   attr_accessor :test_name, :analytes
 
   class << self
+    def all
+      url = 'http://localhost:8080/ehrbase/rest/openehr/v1/query/aql'
+      query = { q: "SELECT c FROM EHR e CONTAINS COMPOSITION c WHERE e/ehr_id/value='b3a123d3-86a9-46a9-a8b9-6ac9289abe97'" }
+      response = HTTPClient.get(url, query)
+      JSON.parse(response.body)['rows'].map do |diagnosis|
+        name_value = name_code = name_terminology = onset = diagnostic_certainty = diagnostic_certainty_code = diagnostic_certainty_terminology = nil
+        id = diagnosis[0]['uid']['value'].split(':')[0]
+        diagnosis[0]['content'][0]['data']['items'].each do |item|
+          case item['archetype_node_id']
+          when 'at0002'
+            name_value = item['value']['value']
+            name_terminology = item['value']['defining_code']['terminology_id']['value']
+            name_code = item['value']['defining_code']['code_string']
+          when 'at0073'
+            diagnostic_certainty = item['value']['value']
+            diagnostic_certainty_code = item['value']['defining_code']['code_string']
+            diagnostic_certainty_terminology = item['value']['defining_code']['terminology_id']['value']
+          when 'at0077'
+            onset = item['value']['value']
+          end
+        end
+        new(id:, onset:, name_value:, name_terminology:, name_code:,
+            diagnostic_certainty:, diagnostic_certainty_code:, diagnostic_certainty_terminology:)
+    end
+
     def from_fhir_json(json)
       fhir = FHIR.from_contents(json)
       LaboratoryTestReport.new(
@@ -28,8 +53,9 @@ class LaboratoryTestReport
 
     def from_mml(xml)
       mml = Nokogiri::XML(xml)
+      print mml.xpath('/Mml')
       LaboratoryTestReport.new(
-        test_name: mml.xpath('/mmlLb:TestModule/mmlLb:information/mmlLb:set').text
+        test_name: mml.xpath('/Mml/MmlBody/MmlModuleItem/docInfo/title').text
       )
     end
   end
--- spec/models/laboratory_test_report_spec.rb
+++ spec/models/laboratory_test_report_spec.rb
@@ -508,52 +508,139 @@ JSON
 
 MML = <<XML
 <?xml version="1.0" encoding="UTF-8"?>
-<mmlLb:TestModule
-    xmlns:mmlLb="http://www.medxml.net/MML/v4/ContentModule/test/1.0"
-    xmlns:mmlCm="http://www.medxml.net/MML/v4/SharedComponent/Common/1.0"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.medxml.net/MML/v4/ContentModule/test/1.0/testhistory.xsd">
-    <mmlLb:information mmlLb:registId="0002228" mmlLb:registTime="2024-06-06T08:30:00+09:00" mmlLb:reportTime="2024-06-06T08:30:00+09:00">
-      <mmlLb:set mmlLb:setCode="11502-2" mmlLb:setCodeId="http://jpfhir.jp/fhir/core/CodeSystem/JP_DocumentCodes_CS">検体検査報告書</mmlLb:set>
-      <mmlLb:reportStatus mmlLb:statusCode="final" mmlLb:statusCodeId="mmlLB0001">最終報告</mmlLb:reportStatus>
-      <mmlLb:facility mmlLb:facilityCode="JPN432101234567" mmlLb:facilityCodeId="JMARI">Nagara Hospital</mmlLb:facility>
-      <mmlLb:client mmlLb:clientCode="facility" mmlLb:clientCodeId="JPN432101234567">87654321</mmlLb:client>
-      <mmlLb:laboratoryCenter mmlLb:centerCode="JPN432101234567" mmlLb:centerCodeId="JMARI">Nagara hospital</mmlLb:laboratoryCenter>
-    </mmlLb:information>
-    <mmlLb:laboTest>
-      <mmlLb:specimen>
-        <mmlLb:specimenName>Specimen/jp-specimen-example-2</mmlLb:specimenName>
-      </mmlLb:specimen>
-      <mmlLb:item>
-        <mmlLb:itemName mmlLb:itCode="26464-8" mmlLb:itCodeId="LOINC">白血球数</mmlLb:itemName>
-        <mmlLb:value>6.8 10*3/uL/mmlLb:value>
-        <mmlLb:numValue mmlLb:up="9.8" mmlLb:low="3.2">6.8</mmlLb:numValue>
-        <mmlLb:unit>10*3/uL</mmlLb:unit>
-      </mmlLb:item>
-      <mmlLb:item>
-        <mmlLb:itemName mmlLb:itCode="26453-1" mmlLb:itCodeId="LOINC">赤血球数</mmlLb:itemName>
-        <mmlLb:value>4.05 10*6/uL</mmlLb:value>
-        <mmlLb:numValue mmlLb:up="5.7" mmlLb:low="4.0">4.05</mmlLb:numValue>
-        <mmlLb:unit>10*6/uL</mmlLb:unit>
-      </mmlLb:item>
-      <mmlLb:item>
-        <mmlLb:itemName mmlLb:itCode="291" mmlLb:itCodeId="LOINC">血色素量</mmlLb:itemName>
-        <mmlLb:value>13.0 g/dL</mmlLb:value>
-        <mmlLb:numValue mmlLb:up="17" mmlLb:low="13.0">13.0</mmlLb:numValue>
-        <mmlLb:unit>g/dL</mmlLb:unit>
-      </mmlLb:item>
-      <mmlLb:item>
-        <mmlLb:itemName mmlLb:itCode="20570-8" mmlLb:itCodeId="LOINC">ヘマトクリット</mmlLb:itemName>
-        <mmlLb:value>40.1 %</mmlLb:value>
-        <mmlLb:numValue mmlLb:up="51.8" mmlLb:low="38.5">140.1</mmlLb:numValue>
-        <mmlLb:unit>%</mmlLb:unit>
-      </mmlLb:item>
-      <mmlLb:item>
-        <mmlLb:itemName mmlLb:itCode="26515-7" mmlLb:itCodeId="LOINC">血小板数</mmlLb:itemName>
-        <mmlLb:value>309 10*3/uL</mmlLb:value>
-        <mmlLb:numValue mmlLb:up="450" mmlLb:low="120">309</mmlLb:numValue>
-        <mmlLb:unit>10*3/uL</mmlLb:unit>
-      </mmlLb:item>
-    </mmlLb:laboTest>
-</mmlLb:TestModule>
+<Mml version="4.2.0"
+     createDate="2016-12-04T19:41:11"
+     xmlns:xhtml="http://www.w3.org/1999/xhtml"
+     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+     xmlns="http://www.medxml.net/MML/v4/base/1.0"
+     xmlns:mml="http://www.medxml.net/MML/v4/base/1.0"
+     xmlns:mmlCm="http://www.medxml.net/MML/v4/SharedComponent/Common/1.0"
+     xmlns:mmlNm="http://www.medxml.net/MML/v4/SharedComponent/Name/1.0"
+     xmlns:mmlFc="http://www.medxml.net/MML/v4/SharedComponent/Facility/1.0"
+     xmlns:mmlDp="http://www.medxml.net/MML/v4/SharedComponent/Department/1.0"
+     xmlns:mmlAd="http://www.medxml.net/MML/v4/SharedComponent/Address/1.0"
+     xmlns:mmlPh="http://www.medxml.net/MML/v4/SharedComponent/Phone/1.0"
+     xmlns:mmlPsi="http://www.medxml.net/MML/v4/SharedComponent/PersonalizedInfo/1.0"
+     xmlns:mmlCi="http://www.medxml.net/MML/v4/SharedComponent/CreatorInfo/1.0"
+     xmlns:mmlPi="http://www.medxml.net/MML/v4/ContentModule/PatientInfo/1.0"
+     xmlns:mmlBc="http://www.medxml.net/MML/v4/ContentModule/BaseClinic/1.0"
+     xmlns:mmlFcl="http://www.medxml.net/MML/v4/ContentModule/FirstClinic/1.0"
+     xmlns:mmlHi="http://www.medxml.net/MML/v4/SharedComponent/HealthInsurance/1.0"
+     xmlns:mmlLs="http://www.medxml.net/MML/v4/ContentModule/Lifestyle/1.0"
+     xmlns:mmlPc="http://www.medxml.net/MML/v4/ContentModule/ProgressCourse/1.0"
+     xmlns:mmlRd="http://www.medxml.net/MML/v4/SharedComponent/RegisteredDiagnosis/1.0"
+     xmlns:mmlSg="http://www.medxml.net/MML/v4/ContentModule/Surgery/1.0"
+     xmlns:mmlSm="http://www.medxml.net/MML/v4/ContentModule/Summary/1.0"
+     xmlns:mmlLb="http://www.medxml.net/MML/v4/ContentModule/test/1.0"
+     xmlns:mmlRp="http://www.medxml.net/MML/v4/ContentModule/report/1.0"
+     xmlns:mmlRe="http://www.medxml.net/MML/v4/ContentModule/Referral/1.0"
+     xmlns:mmlSc="http://www.medxml.net/MML/v4/SharedComponent/Security/1.0"
+     xmlns:claim="http://www.medxml.net/claim/claimModule/2.1"
+     xmlns:claimA="http://www.medxml.net/claim/claimAmountModule/2.1">
+  <MmlHeader>
+    <mmlCi:CreatorInfo>
+      <mmlPsi:PersonalizedInfo>
+	      <mmlCm:Id mmlCm:type="facility" mmlCm:tableId="JPN999999900009">11</mmlCm:Id>
+	      <mmlPsi:personName>
+	        <mmlNm:Name mmlNm:repCode="I" mmlNm:tableId="MML0025">
+	          <mmlNm:family>責任者姓</mmlNm:family>
+	          <mmlNm:given>責任者名</mmlNm:given>
+	        </mmlNm:Name>
+	      </mmlPsi:personName>
+	      <mmlFc:Facility>
+	        <mmlFc:name mmlFc:repCode="I" mmlFc:tableId="MML0025">Nagara Hospital</mmlFc:name>
+	        <mmlCm:Id mmlCm:type="JMARI" mmlCm:tableId="MML0027">JPN999999900009</mmlCm:Id>
+	      </mmlFc:Facility>
+      </mmlPsi:PersonalizedInfo>
+      <mmlCi:creatorLicense mmlCi:tableId="MML0026">lab</mmlCi:creatorLicense>
+    </mmlCi:CreatorInfo>
+    <masterId>
+      <mmlCm:Id mmlCm:type="facility" mmlCm:tableId="JPN999999900099">11370</mmlCm:Id>
+    </masterId>
+  </MmlHeader>
+  <MmlBody>
+    <MmlModuleItem>
+      <docInfo contentModuleType="test">
+      	<mmlSc:securityLevel>
+	        <mmlSc:accessRight permit="read">
+	          <mmlSc:facility>
+	            <mmlSc:facilityName mmlSc:facilityCode="individual" mmlSc:tableId="MML0035" mmlSc:facilityId="JPN99999900099" mmlSc:facilityIdType="JMARI">Nagara Hospital</mmlSc:facilityName>
+	          </mmlSc:facility>
+	        </mmlSc:accessRight>
+	        <mmlSc:accessRight permit="read">
+	          <mmlSc:person>
+	            <mmlSc:personName mmlSc:personCode="patient" mmlSc:tableId="MML0036">患者氏名</mmlSc:personName>
+	          </mmlSc:person>
+	        </mmlSc:accessRight>
+	      </mmlSc:securityLevel>
+      	<title generationPurpose="reportTest">検体検査報告書</title>
+	      <docId>
+	        <uid>b9b5008e-a3fe-4657-8c50-7c9964b6e60d</uid>
+	      </docId>
+    	  <confirmDate>2016-12-04T18:29:33</confirmDate>
+	      <mmlCi:CreatorInfo>
+	        <mmlPsi:PersonalizedInfo>
+	          <mmlCm:Id mmlCm:type="facility" mmlCm:tableId="JPN999999900009">11</mmlCm:Id>
+	          <mmlPsi:personName>
+	            <mmlNm:Name mmlNm:repCode="I" mmlNm:tableId="MML0025">
+		            <mmlNm:family>責任者姓</mmlNm:family>
+		            <mmlNm:given>責任者名</mmlNm:given>
+	            </mmlNm:Name>
+	          </mmlPsi:personName>
+	          <mmlFc:Facility>
+	            <mmlFc:name mmlFc:repCode="I" mmlFc:tableId="MML0025">検査センター</mmlFc:name>
+	            <mmlCm:Id mmlCm:type="JMARI" mmlCm:tableId="MML0027">JPN999999900009</mmlCm:Id>
+	          </mmlFc:Facility>
+	        </mmlPsi:PersonalizedInfo>
+	        <mmlCi:creatorLicense mmlCi:tableId="MML0026">lab</mmlCi:creatorLicense>
+	      </mmlCi:CreatorInfo>
+	      <extRefs />
+      </docInfo>
+      <content>
+        <mmlLb:TestModule>
+          <mmlLb:information mmlLb:registId="0002228" mmlLb:registTime="2024-06-06T08:30:00+09:00" mmlLb:reportTime="2024-06-06T08:30:00+09:00">
+            <mmlLb:set>血算</mmlLb:set>
+              <mmlLb:reportStatus mmlLb:statusCode="final" mmlLb:statusCodeId="mmlLB0001">最終報告</mmlLb:reportStatus>
+            <mmlLb:facility>Nagara Hospital</mmlLb:facility>
+          </mmlLb:information>
+          <mmlLb:laboTest>
+            <mmlLb:specimen>
+              <mmlLb:specimenName>Specimen/jp-specimen-example-2</mmlLb:specimenName>
+            </mmlLb:specimen>
+            <mmlLb:item>
+              <mmlLb:itemName mmlLb:itCode="26464-8" mmlLb:itCodeId="LOINC">白血球数</mmlLb:itemName>
+              <mmlLb:value>6.8 10*3/uL</mmlLb:value>
+              <mmlLb:numValue mmlLb:up="9.8" mmlLb:low="3.2">6.8</mmlLb:numValue>
+             <mmlLb:unit>10*3/uL</mmlLb:unit>
+            </mmlLb:item>
+            <mmlLb:item>
+              <mmlLb:itemName mmlLb:itCode="26453-1" mmlLb:itCodeId="LOINC">赤血球数</mmlLb:itemName>
+              <mmlLb:value>4.05 10*6/uL</mmlLb:value>
+              <mmlLb:numValue mmlLb:up="5.7" mmlLb:low="4.0">4.05</mmlLb:numValue>
+              <mmlLb:unit>10*6/uL</mmlLb:unit>
+            </mmlLb:item>
+            <mmlLb:item>
+              <mmlLb:itemName mmlLb:itCode="291" mmlLb:itCodeId="LOINC">血色素量</mmlLb:itemName>
+              <mmlLb:value>13.0 g/dL</mmlLb:value>
+              <mmlLb:numValue mmlLb:up="17" mmlLb:low="13.0">13.0</mmlLb:numValue>
+              <mmlLb:unit>g/dL</mmlLb:unit>
+            </mmlLb:item>
+            <mmlLb:item>
+              <mmlLb:itemName mmlLb:itCode="20570-8" mmlLb:itCodeId="LOINC">ヘマトクリット</mmlLb:itemName>
+              <mmlLb:value>40.1 %</mmlLb:value>
+              <mmlLb:numValue mmlLb:up="51.8" mmlLb:low="38.5">140.1</mmlLb:numValue>
+              <mmlLb:unit>%</mmlLb:unit>
+            </mmlLb:item>
+            <mmlLb:item>
+              <mmlLb:itemName mmlLb:itCode="26515-7" mmlLb:itCodeId="LOINC">血小板数</mmlLb:itemName>
+              <mmlLb:value>309 10*3/uL</mmlLb:value>
+              <mmlLb:numValue mmlLb:up="450" mmlLb:low="120">309</mmlLb:numValue>
+              <mmlLb:unit>10*3/uL</mmlLb:unit>
+            </mmlLb:item>
+          </mmlLb:laboTest>
+        </mmlLb:TestModule>
+      </content>
+    </MmlModuleItem>
+  </MmlBody>
+</Mml>
 XML
